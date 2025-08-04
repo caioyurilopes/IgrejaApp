ad02eda1a32e09285f972116e8077d6418fe1dd9
Caio Lopes <caioyurilopes@gmail.com>
2025-08-04 07:57:59 -0300

    deploy

---
+++ IgrejaApp/service-worker-assets.js
@@ -0,0 +1,237 @@
+self.assetsManifest = {
+  "version": "wJ1KlJn8",
+  "assets": [
+    {
+      "hash": "sha256-lYX8siEnBOHXXCJHZ9lQ9SuSxDQMacV4G8W60cWRY+g=",
+      "url": "IgrejaApp/IgrejaApp.UI.styles.css"
+    },
+    {
+      "hash": "sha256-lA1PY6ljwcKk0sQwxrAnNV1+O1wyhy9NayTkR7rtayk=",
+      "url": "IgrejaApp/_framework/IgrejaApp.Domain.wasm"
+    },
+    {
+      "hash": "sha256-x0jwt5GgLHoqAX90Z/SW57YEpkl+Jqb9sJ7Rff8qmIs=",
+      "url": "IgrejaApp/_framework/IgrejaApp.UI.wasm"
+    },
+    {
+      "hash": "sha256-Ofll4pl1kC0ckVv8T7MSkTV7bkjwJI6ALVV0Gqt+Ckw=",
+      "url": "IgrejaApp/_framework/Microsoft.AspNetCore.Components.Forms.wasm"
+    },
+    {
+      "hash": "sha256-rXjrArSBEXtEP3KTyRRXanX0LrvwGyC4CWpt8f9icTQ=",
+      "url": "IgrejaApp/_framework/Microsoft.AspNetCore.Components.Web.wasm"
+    },
+    {
+      "hash": "sha256-GPD3uor50dVTreLCV1djKcHB96VmgAWxRMAC7hAUx5Q=",
+      "url": "IgrejaApp/_framework/Microsoft.AspNetCore.Components.WebAssembly.wasm"
+    },
+    {
+      "hash": "sha256-2aE9vJzzKyCr0dn9SNNaLHphBMk863rwKk6HgZkF9SU=",
+      "url": "IgrejaApp/_framework/Microsoft.AspNetCore.Components.wasm"
+    },
+    {
+      "hash": "sha256-XR+WUfg8LtnrwdXFBnV+qO3ROyp6cYCfl1AZajIAheM=",
+      "url": "IgrejaApp/_framework/Microsoft.Extensions.Configuration.Abstractions.wasm"
+    },
+    {
+      "hash": "sha256-Nbv4DDWJLbUri+9QrX+3vnA2FV/Qux8Jm1buTARMlBs=",
+      "url": "IgrejaApp/_framework/Microsoft.Extensions.Configuration.Json.wasm"
+    },
+    {
+      "hash": "sha256-od574rRtVVRJj+24jbl8j3GdQFpxlJOpSx+pEMw1Q18=",
+      "url": "IgrejaApp/_framework/Microsoft.Extensions.Configuration.wasm"
+    },
+    {
+      "hash": "sha256-YplprZeRfkxl7NRcSejpNDMaJdkevGGuL/aHwF5BfR4=",
+      "url": "IgrejaApp/_framework/Microsoft.Extensions.DependencyInjection.Abstractions.wasm"
+    },
+    {
+      "hash": "sha256-5wLKIEWtlGqIs5hP7jsDPajACsIMYJapjnvJ0KoBk7Q=",
+      "url": "IgrejaApp/_framework/Microsoft.Extensions.DependencyInjection.wasm"
+    },
+    {
+      "hash": "sha256-KOwfQHIjclTDhdIKS+xmd+aKXqagT0opz/L023JIFy0=",
+      "url": "IgrejaApp/_framework/Microsoft.Extensions.Logging.Abstractions.wasm"
+    },
+    {
+      "hash": "sha256-KCdgvZvpkT0QTlCjj/PtlVlJnNw1mDlJAtaRSWXz0YM=",
+      "url": "IgrejaApp/_framework/Microsoft.Extensions.Logging.wasm"
+    },
+    {
+      "hash": "sha256-mngxZ7DtB5tYzmxZdVWZapJTSJ/yGClqQAcqOkLk3Lk=",
+      "url": "IgrejaApp/_framework/Microsoft.Extensions.Options.wasm"
+    },
+    {
+      "hash": "sha256-xyqqv0SMCahnrOs+p5QYDHoh/BAYlbNtyjTTpqmRW/g=",
+      "url": "IgrejaApp/_framework/Microsoft.Extensions.Primitives.wasm"
+    },
+    {
+      "hash": "sha256-Nde97m8sZyz01NwVHDOJiS8YTnjxsDgzGTQGZ3ZxJ0w=",
+      "url": "IgrejaApp/_framework/Microsoft.JSInterop.WebAssembly.wasm"
+    },
+    {
+      "hash": "sha256-FeT3eDjqvzYcjqhIDvvsv/iC0wDqLeu235YDwc0EOog=",
+      "url": "IgrejaApp/_framework/Microsoft.JSInterop.wasm"
+    },
+    {
+      "hash": "sha256-xc3AFRJRFeOfnUiHQczbkcKHZUm5K1WD+LiLO1rhz1A=",
+      "url": "IgrejaApp/_framework/System.Collections.Concurrent.wasm"
+    },
+    {
+      "hash": "sha256-0jjAKQE8AI9Z8pM+l9giojhaoXmXhxv0jCkZl+b6g3k=",
+      "url": "IgrejaApp/_framework/System.Collections.NonGeneric.wasm"
+    },
+    {
+      "hash": "sha256-JDwWFZP6VKUikuiYR77oR/60arIZg2agB3oAL/hwdMQ=",
+      "url": "IgrejaApp/_framework/System.Collections.Specialized.wasm"
+    },
+    {
+      "hash": "sha256-KM6thsdVAk2rxOyONSKbY2J4chWs2h8M1y+YZXPPuac=",
+      "url": "IgrejaApp/_framework/System.Collections.wasm"
+    },
+    {
+      "hash": "sha256-D8eA/ugDhdTQKdjK10/mxhhpDkS5W93TcJkgiOFt8/Y=",
+      "url": "IgrejaApp/_framework/System.ComponentModel.Primitives.wasm"
+    },
+    {
+      "hash": "sha256-D9u1vGspaUMBpJY/n1vdrqp0lxo0xktkJX/sv5aawP8=",
+      "url": "IgrejaApp/_framework/System.ComponentModel.TypeConverter.wasm"
+    },
+    {
+      "hash": "sha256-lRbXroqpgiwkr4tkgvfvB62KcB02uGaIE18tqI0d/iY=",
+      "url": "IgrejaApp/_framework/System.ComponentModel.wasm"
+    },
+    {
+      "hash": "sha256-uG029AozXuSq3Ok7kTxoMuJy6+rK6gmEtq7+Gsqi7dE=",
+      "url": "IgrejaApp/_framework/System.Console.wasm"
+    },
+    {
+      "hash": "sha256-v1/0W98pJAKs40Ua8kO/nAn527PVAY1n0jfyYmrZtvE=",
+      "url": "IgrejaApp/_framework/System.Diagnostics.DiagnosticSource.wasm"
+    },
+    {
+      "hash": "sha256-N776F2hbcqZp4xwpodirVRB0Nlgpy9qHcsGuTU4LNPo=",
+      "url": "IgrejaApp/_framework/System.Linq.Expressions.wasm"
+    },
+    {
+      "hash": "sha256-7cYsr9MvqAPWo2AehcjpklMWPBWC7W9zj8Ary8ARf80=",
+      "url": "IgrejaApp/_framework/System.Linq.wasm"
+    },
+    {
+      "hash": "sha256-R78OKA6Pbx4PRI5T3Fxi0q/bfSGcMz4JHi/r0X4DfIE=",
+      "url": "IgrejaApp/_framework/System.Memory.wasm"
+    },
+    {
+      "hash": "sha256-zuqQo0Nf9KP7LRewivoqZ51LZCW0qIZVqZHRG4ZXjLM=",
+      "url": "IgrejaApp/_framework/System.Net.Http.wasm"
+    },
+    {
+      "hash": "sha256-gkZfFGrjgg6qS9h0vSysbuP2SKdFdqVW52K8l5w5yGY=",
+      "url": "IgrejaApp/_framework/System.Net.Primitives.wasm"
+    },
+    {
+      "hash": "sha256-Ljl+024PdVkrTfLj1YLC1Hj+cvm9x8un4d5sgN0/yys=",
+      "url": "IgrejaApp/_framework/System.ObjectModel.wasm"
+    },
+    {
+      "hash": "sha256-+98uWfg4JitMWXci7AUIeFJMggKPDFNR+/6X/N/SOUI=",
+      "url": "IgrejaApp/_framework/System.Private.CoreLib.wasm"
+    },
+    {
+      "hash": "sha256-7ziFiAvz5X0Pzhggw1agXpAjqMK0eskeEZFZKe0HwVE=",
+      "url": "IgrejaApp/_framework/System.Private.Uri.wasm"
+    },
+    {
+      "hash": "sha256-JzU4flvPA860j7hskxU8cL5Pt/jaIoczxFLaQ2YTgjk=",
+      "url": "IgrejaApp/_framework/System.Runtime.InteropServices.JavaScript.wasm"
+    },
+    {
+      "hash": "sha256-7poSABrrfvBFQ2ARdgp4RCU0iAHJWcpDZ0EjcALVDEs=",
+      "url": "IgrejaApp/_framework/System.Runtime.Serialization.Primitives.wasm"
+    },
+    {
+      "hash": "sha256-iMSJeUEuiJ7vRhzTRL8TptKJ9mUhP2u+o4FIMGZnPSE=",
+      "url": "IgrejaApp/_framework/System.Runtime.wasm"
+    },
+    {
+      "hash": "sha256-TxId0lcB72W7M6URyCSyALsnP6TpMOt7+sQfz5jfiwQ=",
+      "url": "IgrejaApp/_framework/System.Text.Encodings.Web.wasm"
+    },
+    {
+      "hash": "sha256-BkyqM2Z7TuCT8IiObMASaufFe+SabCdfApUBBjVd3rw=",
+      "url": "IgrejaApp/_framework/System.Text.Json.wasm"
+    },
+    {
+      "hash": "sha256-Am2972il2xJskpU2BK6RCbP/iGUC3Hj9R5fi8kUD+r8=",
+      "url": "IgrejaApp/_framework/System.Text.RegularExpressions.wasm"
+    },
+    {
+      "hash": "sha256-n/f5afy4u9cEBgTcizWUZ+dkLRFdsZkcUvyhNiEyiq8=",
+      "url": "IgrejaApp/_framework/System.wasm"
+    },
+    {
+      "hash": "sha256-4wcHF68QVLliZc1sLb9tMNyb0tryG0vQ7LaG1O6gm28=",
+      "url": "IgrejaApp/_framework/blazor.boot.json"
+    },
+    {
+      "hash": "sha256-gvHfnndfEu1tRf0rFb5988rWq7ITIotOaE8+AMbKYbc=",
+      "url": "IgrejaApp/_framework/blazor.webassembly.js"
+    },
+    {
+      "hash": "sha256-4BM/t8p0DnD4qMI6D0uGoZUfeuUpEY8MsJd0i5FO/DY=",
+      "url": "IgrejaApp/_framework/dotnet.js"
+    },
+    {
+      "hash": "sha256-CN/vF6YK/IjIjXXCM3Bff6cZ6tPiDyjorgmgjDrE55Y=",
+      "url": "IgrejaApp/_framework/dotnet.native.js"
+    },
+    {
+      "hash": "sha256-E4n1QG0+kvO9XYkmC6Et4HtzyInRfXjByQswYTftmPE=",
+      "url": "IgrejaApp/_framework/dotnet.native.wasm"
+    },
+    {
+      "hash": "sha256-QVLJL2PFoMyGwMOZIRShe4/0TJQfK2WMqoIYdVaI+yM=",
+      "url": "IgrejaApp/_framework/dotnet.runtime.js"
+    },
+    {
+      "hash": "sha256-SZLtQnRc0JkwqHab0VUVP7T3uBPSeYzxzDnpxPpUnHk=",
+      "url": "IgrejaApp/_framework/icudt_CJK.dat"
+    },
+    {
+      "hash": "sha256-8fItetYY8kQ0ww6oxwTLiT3oXlBwHKumbeP2pRF4yTc=",
+      "url": "IgrejaApp/_framework/icudt_EFIGS.dat"
+    },
+    {
+      "hash": "sha256-L7sV7NEYP37/Qr2FPCePo5cJqRgTXRwGHuwF5Q+0Nfs=",
+      "url": "IgrejaApp/_framework/icudt_no_CJK.dat"
+    },
+    {
+      "hash": "sha256-ej/9g6D6tP5ubS7VfpTYyzjYemL8mEzcS/tNNiVZiUI=",
+      "url": "IgrejaApp/appsettings.json"
+    },
+    {
+      "hash": "sha256-ayf9o4jnr+p6+jlsr0+BkYS774Cg/75lsNtGJ5g/8q8=",
+      "url": "IgrejaApp/css/themes.css"
+    },
+    {
+      "hash": "sha256-4mWsDy3aHl36ZbGt8zByK7Pvd4kRUoNgTYzRnwmPHwg=",
+      "url": "IgrejaApp/favicon.png"
+    },
+    {
+      "hash": "sha256-DbpQaq68ZSb5IoPosBErM1QWBfsbTxpJqhU0REi6wP4=",
+      "url": "IgrejaApp/icon-192.png"
+    },
+    {
+      "hash": "sha256-oEo6d+KqX5fjxTiZk/w9NB3Mi0+ycS5yLwCKwr4IkbA=",
+      "url": "IgrejaApp/icon-512.png"
+    },
+    {
+      "hash": "sha256-5TNIRTEavJWKZ3Rd/pz4ysm+qGo1dopqUNLovM3s7fM=",
+      "url": "IgrejaApp/index.html"
+    },
+    {
+      "hash": "sha256-kxJFQwWJgs1flT3z4QvzL9X0TdbY1fE+uJjs7OP0YYg=",
+      "url": "IgrejaApp/manifest.webmanifest"
+    }
+  ]
+};
